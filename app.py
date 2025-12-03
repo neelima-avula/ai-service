935ed58a258e7ff6ef1f1040d694eefe8cba6e85
Neelima Avula <neelima@MacBook-Pro-3.local>
2025-12-02 20:33:09 -0500

    added gemini third commit 4

--- app.py
+++ app.py
@@ -1,38 +1,22 @@
-from fastapi import FastAPI
+rom fastapi import FastAPI
 from pydantic import BaseModel
+from genai import Client
+from genai.schema import *
+
 import os
-import requests
 
 app = FastAPI()
 
-API_KEY = os.environ["GEMINI_API_KEY"]
-MODEL = "gemini-1.5-flash"
+# DO NOT PUT KEY HERE — Railway env will inject it
+client = Client(api_key=os.environ["GEMINI_API_KEY"])
 
 class PromptRequest(BaseModel):
     prompt: str
 
 @app.post("/ai")
 def ai_response(req: PromptRequest):
-
-    url = f"https://generativelanguage.googleapis.com/v1/models/{MODEL}:generateContent?key={API_KEY}"
-
-    payload = {
-        "contents": [
-            {
-                "parts": [
-                    {"text": req.prompt}
-                ]
-            }
-        ]
-    }
-
-    response = requests.post(url, json=payload)
-
-    try:
-        response.raise_for_status()
-    except:
-        return {"error": response.text}
-
-    data = response.json()
-    reply = data["candidates"][0]["content"]["parts"][0]["text"]
-    return {"reply": reply}
+    response = client.models.generate_content(
+        model="gemini-1.5-flash",
+        contents=req.prompt
+    )
+    return {"reply": response.output_text}
